c690549755c6fb1aa80d31de5cdf2a90d7ed84a4
Emile Mathieu <emilem@bamlab.fr>
2017-02-22 14:00:05 +0100

    automatically save cnn weight and score

--- code/learn_features_cnn_mllib.py
+++ code/learn_features_cnn_mllib.py
@@ -1,3 +1,45 @@
+#%% Load DATA
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+X = pd.read_csv('../data/Xtr.csv', header=None).as_matrix()[:, 0:-1].astype(np.float32)
+y = pd.read_csv('../data/Ytr.csv').as_matrix()[:,1]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+X = X.reshape(-1, 3, 32, 32)
+X_train = X_train.reshape(-1, 3, 32, 32)
+X_test = X_test.reshape(-1, 3, 32, 32)
+
+#%% Score on training and test datasets
+
+def score(algo):
+    correct_train = 0
+    total_train = 0
+    N = X_train.shape[0]
+    nb_batchs = int(N / batch_size)
+    for i in range(nb_batchs):
+        inputs = X_train[i*batch_size:(i+1)*batch_size,:]
+        labels = y_train[i*batch_size:(i+1)*batch_size]
+        outputs = algo(inputs)
+        predicted = np.argmax(outputs.data, axis=1)
+        total_train += len(labels)
+        correct_train += (predicted == labels).sum()
+    
+    correct_test = 0
+    total_test = 0
+    N = X_test.shape[0]
+    nb_batchs = int(N / batch_size)
+    for i in range(nb_batchs):
+        inputs = X_test[i*batch_size:(i+1)*batch_size,:]
+        labels = y_test[i*batch_size:(i+1)*batch_size]
+        outputs = algo(inputs)
+        predicted = np.argmax(outputs.data, axis=1)
+        total_test += len(labels)
+        correct_test += (predicted == labels).sum()
+    
+    return round(100*correct_train/total_train,2), round(100*correct_test/total_test,2)
+
 #%%
 from mllib import nn
 
@@ -8,7 +50,7 @@ class MyNet(nn.Module):
             nn.Conv2d(3, 6, 5),
             nn.ReLU(),
             nn.MaxPool2d(2,2),
-            nn.Conv2d(6, 16, 5),
+            nn.Conv2d(6, 12, 5),
             nn.ReLU(),
             nn.MaxPool2d(2,2)
         )
@@ -16,19 +58,19 @@ class MyNet(nn.Module):
         #self.flatten = nn.Flatten()
         
         self.classifier = nn.Sequential(
-                nn.Linear(16*5*5, 10)
+                nn.Linear(12*5*5, 10)
         )
     def forward(self, x):
         x = self.features(x)
         #x = self.flatten(x)
-        x = x.reshape(-1, 16*5*5)
+        x = x.reshape(-1, 12*5*5)
         x = self.classifier(x)
         return x.reshape(x.shape[0],-1)
 
     def backward(self, output_grad):
         output_grad = self.classifier.backward(output_grad)
         #output_grad = self.flatten.backward(output_grad)
-        output_grad = output_grad.reshape(-1, 16, 5, 5)
+        output_grad = output_grad.reshape(-1, 12, 5, 5)
         return self.features.backward(output_grad)    
 
     def step(self, optimizer):
@@ -57,22 +99,15 @@ mynet = MyNet()
 #mynet.classifier._modules[0]._weight = parameters[4].copy()
 #mynet.classifier._modules[0]._bias = parameters[5].copy()
 
-#%% Load DATA
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-
-X = pd.read_csv('../data/Xtr.csv', header=None).as_matrix()[:, 0:-1].astype(np.float32)
-y = pd.read_csv('../data/Ytr.csv').as_matrix()[:,1]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-
-X = X.reshape(-1, 3, 32, 32)
-X_train = X_train.reshape(-1, 3, 32, 32)
-X_test = X_test.reshape(-1, 3, 32, 32)
 #%% Train
-from importlib import reload
-import timeit
+from importlib import reload 
 from mllib import optim, loss
+import timeit, pickle, json, os
+
+experience_name = '3__depth_12_deterministic_batch'
+directory_path = 'parameters/{}/'.format(experience_name)
+if not os.path.exists(directory_path):
+    os.makedirs(directory_path)
 
 #optimizer = optim.SGD(lr=0.001, momentum=0.9)
 optimizer = optim.Adam(lr=0.001)
@@ -83,12 +118,14 @@ N = X_train.shape[0]
 batch_size = 8
 nb_batchs = int(N / batch_size)
 
+scores = {}
 start_global = timeit.default_timer()
 optimizer.zero_grad()
-for epoch in range(20): # loop over the dataset multiple times
+for epoch in range(0, 40, 1): # loop over the dataset multiple times
     
     running_loss = 0.0
     start = timeit.default_timer()
+    
     for i in range(nb_batchs):
     #for i in np.random.permutation(nb_batchs):
         # get the inputs
@@ -112,37 +149,30 @@ for epoch in range(20): # loop over the dataset multiple times
                     round(timeit.default_timer() - start, 2))
             running_loss = 0.0
             start = timeit.default_timer()
+    if (epoch + 1) % 5 == 0:
+        score_train, score_test = score(mynet)
+        scores[epoch + 1] = (score_train, score_test)
+        print('Accuracy -- Train: {} | Test: {}'.format(score_train, score_test))
+        pickle.dump(mynet.parameters(),
+                    open("parameters/{}/mynet_parameters_{}.p".format(experience_name, epoch+1), "wb"))
 
+with open('{}/scores.json'.format(directory_path), 'w') as outfile:
+    json.dump(scores, outfile)
 print('Finished Training | {} seconds'.format(round(timeit.default_timer() - start_global, 2)))
 
-#%% Score on training and test datasets
-correct_train = 0
-total_train = 0
-N = X_train.shape[0]
-nb_batchs = int(N / batch_size)
-for i in range(nb_batchs):
-    inputs = X_train[i*batch_size:(i+1)*batch_size,:]
-    labels = y_train[i*batch_size:(i+1)*batch_size]
-    outputs = mynet(inputs)
-    predicted = np.argmax(outputs.data, axis=1)
-    total_train += len(labels)
-    correct_train += (predicted == labels).sum()
+#%%
+#for i in range(0,0,20):
+#    print(i)
+i=0
+parameters = pickle.load(open( "parameters/mynet_parameters_{}.p".format(i), "rb" ))
+mynet.features._modules[0]._weight = parameters[0].copy()
+mynet.features._modules[0]._bias = parameters[1].copy()
+mynet.features._modules[3]._weight = parameters[2].copy()
+mynet.features._modules[3]._bias = parameters[3].copy()
+mynet.classifier._modules[0]._weight = parameters[4].copy()
+mynet.classifier._modules[0]._bias = parameters[5].copy()
 
-correct_test = 0
-total_test = 0
-N = X_test.shape[0]
-nb_batchs = int(N / batch_size)
-for i in range(nb_batchs):
-    inputs = X_test[i*batch_size:(i+1)*batch_size,:]
-    labels = y_test[i*batch_size:(i+1)*batch_size]
-    outputs = mynet(inputs)
-    predicted = np.argmax(outputs.data, axis=1)
-    total_test += len(labels)
-    correct_test += (predicted == labels).sum()
-
-print('Accuracy -- Train: {} | Test: {}'.format(
-        round(100*correct_train/total_train,2), round(100*correct_test/total_test,2)))
-
+score(mynet)
 #%%
 import pandas as pd
 
